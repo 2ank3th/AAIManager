f79c230effe452e18ca5830f3749112a765f48b1
Sanketh Purwar <sanketh@zilingo.com>
2016-05-31 19:56:54 +0530

    Initial Commit

---
+++ AAIManager/AAIManager.h
@@ -0,0 +1,19 @@
+//
+//  AAIManager.h
+//  AAIManager
+//
+//  Created by Sanketh Purwar on 5/31/16.
+//  Copyright © 2016 Sanketh Purwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AAIManager.
+FOUNDATION_EXPORT double AAIManagerVersionNumber;
+
+//! Project version string for AAIManager.
+FOUNDATION_EXPORT const unsigned char AAIManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AAIManager/PublicHeader.h>
+
+
